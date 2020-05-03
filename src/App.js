5bf25bca0797397be9a7d68640950557d22454d9
Fajri Abdillah <clasense4@gmail.com>
2020-05-04 01:24:21 +0700

    Add example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor() {
+    super()
+
+    // data provinsi disimpan di state.provinces
+    this.state = {
+      filter: '',
+      provinces: []
+    }
+  }
+
+  componentDidMount() {
+    // ajax call
+    fetch('http://localhost:8000/api/all')
+    .then(response => response.json())
+    .then((json) => {
+      this.setState({
+        provinces: json.data
+      })
+    })
+  }
+
+  filter = () => {
+    fetch('http://localhost:8000/api/get?filter=' + this.state.filter)
+    .then(response => response.json())
+    .then((json) => {
+      this.setState({
+        provinces: json.data
+      })
+    })
+  }
+
+  onChangeText = (event) => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  renderProvinces(item, index) {
+    return <li key={index}>{item.name}</li>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Filter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <input type="text" value={this.state.filter} onChange={this.onChangeText} style={{marginBottom: 8}}/><br/>
+        <button onClick={this.filter}>Saring</button>
+
+        <div style={{display: 'flex', justifyContent: 'center', textAlign: 'left'}}>
+          <ol>
+            { this.state.provinces.map(this.renderProvinces) }
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
